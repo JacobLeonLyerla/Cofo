7ddbe6046d4d9deb15188d0d00abd497fc46db8b
Jacob <jacobleonlyerla@gmail.com>
2020-03-23 17:35:29 -0700

    testing fetch

--- cofo/src/App.js
+++ cofo/src/App.js
@@ -1,8 +1,24 @@
 import React from "react";
 import "./App.css";
-
+function getData () {
+  
+  fetch("https://webcamstravel.p.rapidapi.com/webcams/list/%7Bmodifier%7D?lang=en", {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "webcamstravel.p.rapidapi.com",
+      "x-rapidapi-key": "OIodsvqwbCmshvxVv1Z3Nte6EXDKp1nb8Y9jsnzDUsBqZBEX0s"
+    }
+  })
+  .then(response => {
+    console.log(response);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+  }
 function App() {
-  return <div className="App">test</div>;
+  
+  return <button onClick={()=>getData()} className="App">fetch</button>;
 }
 
 export default App;
