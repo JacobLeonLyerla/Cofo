739d7868efc03841ee653d4db337b979c0fb96dc
Jacob <jacobleonlyerla@gmail.com>
2020-03-24 14:28:28 -0700

    setting up my provider

--- cofo/src/context/countryProvider.js
+++ cofo/src/context/countryProvider.js
@@ -1,28 +1,38 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 import axios from "axios";
 
 export const CountryContext = react.createContext({});
 
 class CountryProvider extends Component {
-
-    state = {
-    }
-     getData = () =>{
-        axios
-        .get("https://restcountries-v1.p.rapidapi.com/all",{
-          headers: {
-            'X-RapidAPI-Key': process.env.REACT_APP_API_KEY
-          }
-        })
-        .then(response =>{
-          console.log(response.data)
-        })
-        .catch(err => {
-          console.log(err);
-        });
+  state = {};
+  getData = () => {
+    axios
+      .get("https://restcountries-v1.p.rapidapi.com/all", {
+        headers: {
+          "X-RapidAPI-Key": process.env.REACT_APP_API_KEY
         }
-    
+      })
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+  render() {
+    return (
+      <CountryContext.Provider
+        value={{
+          actions: {
+            getData: this.getData
+          }
+        }}
+      >
+        {this.props.children}{" "}
+      </CountryContext.Provider>
+    );
+  }
 }
 
 export default CountryProvider;
