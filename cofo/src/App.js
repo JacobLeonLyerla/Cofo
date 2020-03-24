c610b777886274fcea49a39f39292926f5b3b594
Jacob <jacobleonlyerla@gmail.com>
2020-03-24 14:23:30 -0700

    testing .env

--- cofo/src/App.js
+++ cofo/src/App.js
@@ -2,9 +2,12 @@ import React from "react";
 import axios from "axios"
 import "./App.css";
 
+
+
 function App() {
+ 
   
-  return <button onClick={()=>getData()} className="App">fetch</button>;
+  return <button onClick={()=>console.log(process.env.REACT_APP_API_KEY)} className="App">fetch</button>;
 }
 
 export default App;
