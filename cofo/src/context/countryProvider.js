bc3dad9279379b84db3eaf87f94e7481cd4b8224
Jacob <jacobleonlyerla@gmail.com>
2020-03-23 19:51:49 -0700

    added in our call

--- cofo/src/context/countryProvider.js
+++ cofo/src/context/countryProvider.js
@@ -8,6 +8,21 @@ class CountryProvider extends Component {
 
     state = {
     }
+     getData = () =>{
+        axios
+        .get("https://restcountries-v1.p.rapidapi.com/all",{
+          headers: {
+            'X-RapidAPI-Key': "OIodsvqwbCmshvxVv1Z3Nte6EXDKp1nb8Y9jsnzDUsBqZBEX0s"
+          }
+        })
+        .then(response =>{
+          console.log(response.data)
+        })
+        .catch(err => {
+          console.log(err);
+        });
+        }
+    
 }
 
 export default CountryProvider;
