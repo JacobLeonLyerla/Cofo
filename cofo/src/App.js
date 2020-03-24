092164fa97aaeada586a3348042e65c05e5667e0
Jacob <jacobleonlyerla@gmail.com>
2020-03-24 14:34:34 -0700

    Added in consumer

--- cofo/src/App.js
+++ cofo/src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
-import axios from "axios"
 import "./App.css";
 
-
-
+import CountryProvider, { CountryContext } from "./context/countryProvider";
 function App() {
- 
-  
-  return <button onClick={()=>console.log(process.env.REACT_APP_API_KEY)} className="App">fetch</button>;
+  return (
+    <div>
+      <CountryProvider>
+        <CountryContext.Consumer>
+          <button
+            onClick={() => console.log(process.env.REACT_APP_API_KEY)}
+            className="App"
+          >
+            fetch
+          </button>
+          ;
+        </CountryContext.Consumer>
+      </CountryProvider>
+    </div>
+  );
 }
 
 export default App;
