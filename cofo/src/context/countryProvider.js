6ef658eefb1e2f79c23b0ac353bd2ee43be757d7
Jacob <jacobleonlyerla@gmail.com>
2020-03-23 19:48:51 -0700

    context started

---
+++ cofo/src/context/countryProvider.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+import axios from "axios";
+
+export const CountryContext = react.createContext({});
+
+class CountryProvider extends Component {
+
+    state = {
+    }
+}
+
+export default CountryProvider;
