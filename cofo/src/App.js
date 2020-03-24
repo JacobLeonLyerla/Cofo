1d5a8d5eaf9d141b134095c6611c7ae579f6245b
Jacob <jacobleonlyerla@gmail.com>
2020-03-23 19:50:19 -0700

    moved code

--- cofo/src/App.js
+++ cofo/src/App.js
@@ -1,20 +1,7 @@
 import React from "react";
 import axios from "axios"
 import "./App.css";
-function getData () {
-  axios
-  .get("https://restcountries-v1.p.rapidapi.com/all",{
-    headers: {
-      'X-RapidAPI-Key': "OIodsvqwbCmshvxVv1Z3Nte6EXDKp1nb8Y9jsnzDUsBqZBEX0s"
-    }
-  })
-  .then(response =>{
-    console.log(response.data)
-  })
-  .catch(err => {
-    console.log(err);
-  });
-  }
+
 function App() {
   
   return <button onClick={()=>getData()} className="App">fetch</button>;
