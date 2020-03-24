1bd91352f3ff163626148479eee866b2fee26745
Jacob <jacobleonlyerla@gmail.com>
2020-03-23 18:41:49 -0700

    edited fetch

--- cofo/src/App.js
+++ cofo/src/App.js
@@ -3,14 +3,14 @@ import axios from "axios"
 import "./App.css";
 function getData () {
   axios
-  .get("https://webcamstravel.p.rapidapi.com/webcams/list/%7Bmodifier%7D?lang=en",{
+  .get("https://restcountries-v1.p.rapidapi.com/all",{
     headers: {
-      "x-rapidapi-host": "webcamstravel.p.rapidapi.com",
+      "x-rapidapi-host": "estcountries-v1.p.rapidapi.com",
       "x-rapidapi-key": "OIodsvqwbCmshvxVv1Z3Nte6EXDKp1nb8Y9jsnzDUsBqZBEX0s"
     }
   })
   .then(response =>{
-    console.log(response)
+    console.log("did it")
   })
   .catch(err => {
     console.log(err);
