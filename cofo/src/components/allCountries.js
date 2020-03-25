1cb0985cc70947dcfb6e866b6d082e5b77b02c79
Jacob <jacobleonlyerla@gmail.com>
2020-03-25 14:55:30 -0700

    formatting

--- cofo/src/components/allCountries.js
+++ cofo/src/components/allCountries.js
@@ -1,18 +1,19 @@
-import React from 'react'
+import React from "react";
 
-function AllCountries(props){
- 
-function renderAll(){
-   if (props.context.countryData.countries.length <1 || !props.context.countryData.countries) {
-      props.context.actions.getData("https://restcountries-v1.p.rapidapi.com/all")
-    }  
+function AllCountries(props) {
+  function renderAll() {
+    if (
+      props.context.countryData.countries.length < 1 ||
+      !props.context.countryData.countries
+    ) {
+      props.context.actions.getData(
+        "https://restcountries-v1.p.rapidapi.com/all"
+      );
+    }
     const countries = props.context.countryData.countries;
-return countries.map(country => (
-    <div>{country.name}</div>
-))
+    return countries.map(country => <div>{country.name}</div>);
+  }
 
+  return renderAll();
 }
-
-    return renderAll()
-}
-export default AllCountries
+export default AllCountries;
