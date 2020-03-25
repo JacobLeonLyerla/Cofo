08f4d07bf60d057a838e0d70f1759a845020d6e9
Jacob <jacobleonlyerla@gmail.com>
2020-03-25 14:55:24 -0700

    formatting

--- cofo/src/App.js
+++ cofo/src/App.js
@@ -1,34 +1,35 @@
-import React, {Fragment} from "react";
+import React, { Fragment } from "react";
 import "./App.css";
-import { Route,Link } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 
 import CountryProvider, { CountryContext } from "./context/countryProvider";
 
-import AllCountries from "./components/allCountries"
+import AllCountries from "./components/allCountries";
 
 function App() {
   return (
     <div className="App">
       <CountryProvider>
         <CountryContext.Consumer>
-          {context => (<Fragment>
-            <Link
-              onClick={() =>
-                context.actions.getData(
-                  "https://restcountries-v1.p.rapidapi.com/all"
-                )
-              }
-              to="/all"
-             
-            >
-              fetch
-            </Link>
-            <Route
-            exact
-            path="/all"
-            render={()=><AllCountries context={context}/>}
-            />
-          </Fragment>)}
+          {context => (
+            <Fragment>
+              <Link
+                onClick={() =>
+                  context.actions.getData(
+                    "https://restcountries-v1.p.rapidapi.com/all"
+                  )
+                }
+                to="/all"
+              >
+                fetch
+              </Link>
+              <Route
+                exact
+                path="/all"
+                render={() => <AllCountries context={context} />}
+              />
+            </Fragment>
+          )}
         </CountryContext.Consumer>
       </CountryProvider>
     </div>
