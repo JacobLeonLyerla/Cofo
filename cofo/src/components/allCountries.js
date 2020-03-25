5e44c1aa1eeab33d26b74fe01ce3d8a14f76997c
Jacob <jacobleonlyerla@gmail.com>
2020-03-25 14:54:39 -0700

    maping over our array of countries
    Not sure what to do with this data yet

--- cofo/src/components/allCountries.js
+++ cofo/src/components/allCountries.js
@@ -1,7 +1,18 @@
 import React from 'react'
 
 function AllCountries(props){
-console.log(props.context.countryData)
-    return <div></div>
+ 
+function renderAll(){
+   if (props.context.countryData.countries.length <1 || !props.context.countryData.countries) {
+      props.context.actions.getData("https://restcountries-v1.p.rapidapi.com/all")
+    }  
+    const countries = props.context.countryData.countries;
+return countries.map(country => (
+    <div>{country.name}</div>
+))
+
+}
+
+    return renderAll()
 }
 export default AllCountries
