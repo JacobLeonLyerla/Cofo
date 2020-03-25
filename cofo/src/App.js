f6ee1a0cd3df287928aca2d27938d3e072602931
Jacob <jacobleonlyerla@gmail.com>
2020-03-25 14:14:03 -0700

    setting up routes

--- cofo/src/App.js
+++ cofo/src/App.js
@@ -1,17 +1,33 @@
-import React from "react";
+import React, {Fragment} from "react";
 import "./App.css";
+import { Route } from "react-router-dom";
 
 import CountryProvider, { CountryContext } from "./context/countryProvider";
+
+import AllCountries from "./components/allCountries"
+
 function App() {
   return (
     <div>
       <CountryProvider>
         <CountryContext.Consumer>
-          {context => (
-            <button onClick={()=>context.actions.getData('https://restcountries-v1.p.rapidapi.com/all')} className="App">
+          {context => (<Fragment>
+            <button
+              onClick={() =>
+                context.actions.getData(
+                  "https://restcountries-v1.p.rapidapi.com/all"
+                )
+              }
+              className="App"
+            >
               fetch
             </button>
-          )}
+            <Route
+            exact
+            path="/all"
+            render={()=><AllCountries context={context}/>}
+            />
+          </Fragment>)}
         </CountryContext.Consumer>
       </CountryProvider>
     </div>
