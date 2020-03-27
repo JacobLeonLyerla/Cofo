7b88b16890568239650d84d9c54d8a798e89f22c
Jacob <jacobleonlyerla@gmail.com>
2020-03-26 21:57:33 -0700

    search bar logic mostly there

--- cofo/src/components/searchBar.js
+++ cofo/src/components/searchBar.js
@@ -1,8 +1,25 @@
-import React, {Fragment } from "react";
+import React, {Component,Fragment } from "react";
 
 import { handleChange } from "../../helpers/commonHelpers";
 
-function seachBar(){
+class SeachBar extends Component{
+    state={search:''}
+    render(){
+        this.handleChange = handleChange.bind(this);
+
+        return(
+    <input
+    type="search"
+    name="search"
+    id="search"
+    value ={this.state.search}
+    onChange={e =>{
+        this.handleChange(e)}}
+    
+    
+    />
+    )}
+
 
 }
-export default seachBar
+export default SeachBar
