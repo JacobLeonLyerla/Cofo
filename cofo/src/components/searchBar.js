449d5ebf6888dac113adb883fa6b4ee680e9e539
Jacob <jacobleonlyerla@gmail.com>
2020-03-27 17:58:01 -0700

    formatting

--- cofo/src/components/searchBar.js
+++ cofo/src/components/searchBar.js
@@ -1,32 +1,28 @@
 import React, { Component } from "react";
-import {  Input, Button } from "reactstrap";
+import { Input, Button } from "reactstrap";
 
 import { handleChange } from "../helpers/commonHelpers";
-
-
 
 // make it so it does not refreash everytime you search
 class SeachBar extends Component {
   state = { search: "" };
   render() {
     this.handleChange = handleChange.bind(this);
-   function countrySearch() {
-       console.log(this.state)
-    }
+    const countrySearch = () => {
+      console.log(this.state);
+    };
     return (
-        <form   onSubmit={countrySearch} >
-      <Input
-      
-        type="search"
-        name="search"
-        id="search"
-        value={this.state.search}
-        onChange={e => {
-          this.handleChange(e);
-        }}
-      />
-                <Button type="submit">Sign In</Button>
-
+      <form onSubmit={countrySearch}>
+        <Input
+          type="search"
+          name="search"
+          id="search"
+          value={this.state.search}
+          onChange={e => {
+            this.handleChange(e);
+          }}
+        />
+        <Button type="submit">Sign In</Button>
       </form>
     );
   }
