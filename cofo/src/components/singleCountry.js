8e7fecf18e77050fc528227097b9c3f72b27e1bf
Jacob <jacobleonlyerla@gmail.com>
2020-04-03 15:48:32 -0700

    using the all country as a starting point

--- cofo/src/components/singleCountry.js
+++ cofo/src/components/singleCountry.js
@@ -1,8 +1,55 @@
-import React, { Component } from "react";
+import React from "react";
 
-class SingleCountry extends Component {
-  render() {
-    return <div>test</div>;
-  }
-}
-export default SingleCountry;
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserAlt, faBaby } from "@fortawesome/free-solid-svg-icons";
+import { Card, CardText, CardBody, CardTitle, CardSubtitle } from "reactstrap";
+import { Link } from "react-router-dom";
+
+// DEV NOTE fix  it so there is a max height and handle the overflow
+const SingleCountries = props => {
+  // I am using this two places make this into helper
+  const popCalulation = population => {
+    const pop = Math.round(population / 10000000);
+    let popIcons = [];
+    if (pop < 1) return <FontAwesomeIcon icon={faBaby} />;
+    for (let i = 0; i < pop; i++) {
+      popIcons.push(<FontAwesomeIcon icon={faUserAlt} />);
+    }
+    return popIcons.map(icon => icon);
+  };
+  const renderAll = () => {
+    if (
+      props.context.countryData.countries.length < 1 ||
+      !props.context.countryData.countries
+    ) {
+      props.context.actions.getData(
+        "https://restcountries-v1.p.rapidapi.com/all"
+      );
+    }
+    const countries = props.context.countryData.countries;
+    return countries.map(country => (
+      <div className="card-container">
+        {" "}
+        <Link className='link-styles'
+               country={country}
+                to={`/country/${country.name}`}
+              >
+        <Card>
+          <CardBody>
+            <CardTitle>{country.name}</CardTitle>
+            <CardSubtitle>{country.region}</CardSubtitle>
+            <CardText>{popCalulation(country.population)} {country.population > 1000000
+                ? `${(country.population / 1000000).toFixed(1)} Milion`
+                : `${country.population} Total`}
+            </CardText>
+    
+          </CardBody>
+        </Card>
+        </Link>
+      </div>
+    ));
+  };
+
+  return <div className="container">{renderAll()}</div>;
+};
+export default SingleCountries;
