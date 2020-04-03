27ba2e03b42ff21b3b8e09cb324a46ea7c146ab4
Jacob <jacobleonlyerla@gmail.com>
2020-04-02 22:36:39 -0700

    formatting

--- cofo/src/components/singleCountry.js
+++ cofo/src/components/singleCountry.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 
 class SingleCountry extends Component {
-  render(){
-      return<div>test</div>
+  render() {
+    return <div>test</div>;
   }
 }
 export default SingleCountry;
