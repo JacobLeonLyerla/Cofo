1b327becb439eb4f8bf78edcce0e2ec6e9ee3ecf
Jacob <jacobleonlyerla@gmail.com>
2020-04-07 21:32:55 -0700

    imported and added function

--- cofo/src/helpers/countryCard.js
+++ cofo/src/helpers/countryCard.js
@@ -1,24 +1,38 @@
 import React from "react"
 import { Card, CardText, CardBody, CardTitle, CardSubtitle } from "reactstrap";
 import { Link } from "react-router-dom";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserAlt, faBaby,faPlus } from "@fortawesome/free-solid-svg-icons";
 const CountryCard = props =>{
-  console.log(props)
+
+  const popCalulation = population => {
+    const pop = Math.round(population / 10000000);
+    let popIcons = [];
+    if (pop < 1) return <FontAwesomeIcon icon={faBaby} />;
+    for (let i = 0; i < pop; i++) {
+      if(i ===8){
+          popIcons.push(<FontAwesomeIcon icon={faPlus} />)
+          break;
+        }
+      popIcons.push(<FontAwesomeIcon icon={faUserAlt} />);
+    }
+    return popIcons.map(icon => icon);
+  };
 return(
     <Link className='link-styles'
                country={props.country}
                 to={`/country/${props.country.name}`}
               >
         <Card>
-          {/* <CardBody>
-            <CardTitle>{country.name}</CardTitle>
-            <CardSubtitle>{country.region}</CardSubtitle>
-            <CardText>{popCalulation(country.population)} {country.population > 1000000
-                ? `${(country.population / 1000000).toFixed(1)} Milion`
-                : `${country.population} Total`}
+          <CardBody>
+            <CardTitle>{props.country.name}</CardTitle>
+            <CardSubtitle>{props.country.region}</CardSubtitle>
+            <CardText>{popCalulation(props.country.population)} {props.country.population > 1000000
+                ? `${(props.country.population / 1000000).toFixed(1)} Milion`
+                : `${props.country.population} Total`}
             </CardText>
     
-          </CardBody> */}
+          </CardBody>
         </Card>
         </Link>
 )
