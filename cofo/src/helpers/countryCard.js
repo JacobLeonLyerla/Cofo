85189e1ccefff333b7b3355a40d5e3f0509e47c5
Jacob <jacobleonlyerla@gmail.com>
2020-04-07 21:34:56 -0700

    formatting

--- cofo/src/helpers/countryCard.js
+++ cofo/src/helpers/countryCard.js
@@ -1,40 +1,41 @@
-import React from "react"
+import React from "react";
 import { Card, CardText, CardBody, CardTitle, CardSubtitle } from "reactstrap";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUserAlt, faBaby,faPlus } from "@fortawesome/free-solid-svg-icons";
-const CountryCard = props =>{
-
-  const popCalulation = population => {
+import { faUserAlt, faBaby, faPlus } from "@fortawesome/free-solid-svg-icons";
+const CountryCard = (props) => {
+  const popCalulation = (population) => {
     const pop = Math.round(population / 10000000);
     let popIcons = [];
     if (pop < 1) return <FontAwesomeIcon icon={faBaby} />;
     for (let i = 0; i < pop; i++) {
-      if(i ===8){
-          popIcons.push(<FontAwesomeIcon icon={faPlus} />)
-          break;
-        }
+      if (i === 8) {
+        popIcons.push(<FontAwesomeIcon icon={faPlus} />);
+        break;
+      }
       popIcons.push(<FontAwesomeIcon icon={faUserAlt} />);
     }
-    return popIcons.map(icon => icon);
+    return popIcons.map((icon) => icon);
   };
-return(
-    <Link className='link-styles'
-               country={props.country}
-                to={`/country/${props.country.name}`}
-              >
-        <Card>
-          <CardBody>
-            <CardTitle>{props.country.name}</CardTitle>
-            <CardSubtitle>{props.country.region}</CardSubtitle>
-            <CardText>{popCalulation(props.country.population)} {props.country.population > 1000000
-                ? `${(props.country.population / 1000000).toFixed(1)} Milion`
-                : `${props.country.population} Total`}
-            </CardText>
-    
-          </CardBody>
-        </Card>
-        </Link>
-)
-}
-export default CountryCard
+  return (
+    <Link
+      className="link-styles"
+      country={props.country}
+      to={`/country/${props.country.name}`}
+    >
+      <Card>
+        <CardBody>
+          <CardTitle>{props.country.name}</CardTitle>
+          <CardSubtitle>{props.country.region}</CardSubtitle>
+          <CardText>
+            {popCalulation(props.country.population)}{" "}
+            {props.country.population > 1000000
+              ? `${(props.country.population / 1000000).toFixed(1)} Milion`
+              : `${props.country.population} Total`}
+          </CardText>
+        </CardBody>
+      </Card>
+    </Link>
+  );
+};
+export default CountryCard;
