86a2eb95b2ddb8b5951d5dc79143a91308eff855
Jacob <jacobleonlyerla@gmail.com>
2020-04-07 21:40:17 -0700

    added filter so I would not call the api so much

--- cofo/src/components/allCountries.js
+++ cofo/src/components/allCountries.js
@@ -4,10 +4,11 @@ import CountryCard from "../helpers/countryCard";
 // DEV NOTE fix  it so there is a max height and handle the overflow
 const AllCountries = (props) => {
   const renderAll = () => {
+    if(props.context.countries.length <0){
     props.context.actions.getData(
       "https://restcountries-v1.p.rapidapi.com/all"
     );
-
+    }
     const countries = props.context.countryData.countries;
     return countries.map((country) => (
       <div className="card-container">
